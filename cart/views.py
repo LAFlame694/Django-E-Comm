495d905a1869d227e0fa7dea312ad89e3105fc57
LAFlame594 <lilflame694@gmail.com>
2025-07-08 22:04:49 +0300

    Update 2.1

---
+++ cart/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse, HttpResponseBadRequest
+
+# Create your views here.
+def cart_summery(request):
+    return render(request, 'cart_summery.html', {})
+
+def cart_add(request):
+    # Get the cart
+    cart = Cart(request)
+
+    # test for post
+    if request.POST.get('action') == 'post':
+        
+         # Get stuff
+         product_id = int(request.POST.get('product_id'))
+         product_qty = int(request.POST.get('product_qty'))
+        
+         # lookup product in the databse
+         product = get_object_or_404(Product, id=product_id)
+
+         # save to session
+         cart.add(product=product, quantity=product_qty)
+
+         # Get cart quantity
+         cart_quantity = cart.__len__()
+
+         # Return response
+         # response  =JsonResponse({'Product Name:': product.name})
+         response  =JsonResponse({'qty': cart_quantity})
+         return response
+
+def cart_delete(request):
+    pass
+
+def cart_update(request):
+    pass
--- store/views.py
+++ store/views.py
@@ -2,11 +2,34 @@ from django.shortcuts import render, redirect
 from .models import Category, Customer, Product, Order
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 # Create your views here.
+def category(request, foo):
+    # Replace hyphens with space.
+    foo = foo.replace('-', ' ')
+
+    # Grab the category from the url
+    try:
+        # Look up the category
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, 'category': category})
+
+    except:
+        messages.error(request, ("That category doesn't exist!!"))
+        return redirect('home')
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product':product})
+
 def home(request):
-    product = Product.objects.all()
-    return render(request, 'home.html', {'products': product})
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
 
 def about(request):
     return render(request, 'about.html', {})
@@ -30,3 +53,23 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out successfully...Thanks!")
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            # log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have registered successfully, You can now LogIn!"))
+            return redirect('home')
+        else:
+            messages.error(request, ("Whoops! Something went wrong please try again."))
+            return redirect('register')
+        
+    return render(request, 'register.html', {'form': form})
